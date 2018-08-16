d37f49fbf2074186328c010a218f17e1de5f9460
Jennifer Vermorel <jennifer.vermorel@gmail.com>
2018-08-16 16:50:17 +0200

    Mister Cocktail Cocktail Controller done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # A user can see the list of cocktails
+  # A user can see the details of a given cocktail, with the dose needed for each ingredient
+  # A user can create a new cocktail
+
+  get 'cocktails', to: 'cocktails#index'
+
+  get 'cocktails/new', to: 'cocktails#new'
+  get 'cocktails/:id', to: 'cocktails#show'
+
+  post 'cocktails', to: 'cocktails#create'
+
 end
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,6 @@
 const { environment } = require('@rails/webpacker')
 
-const { environment } = require('@rails/webpacker')
-
+// Bootstrap 3 has a dependency over jQuery:
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
@@ -9,6 +8,4 @@ environment.plugins.prepend('Provide',
     jQuery: 'jquery'
   })
 )
-module.exports = environment
-
 module.exports = environment
